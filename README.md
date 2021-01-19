841d08bccd3153b22c5524620a50fac668bd4419
patricio-dsgn <patricio.ngg@gmail.com>
2021-01-19 17:11:00 -0300

    test git 2

--- README.md
+++
@@ -1,2 +0,0 @@
-# yumi
-Esta es una aplicación si no quieres dejar a tu mascota sólo en casa cuando sales.
---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Proyecto Yumi - v1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<br><br>
+
+<div class="container">
+
+	<div class="jumbotron">		
+
+		<h1>Yumi</h1>
+		<p>(Proyecto en desarrollo)</p>
+		<br>
+		<h4>María José Ensignia / cote_ensignia#0167</h4>
+		<h4>Patricio Garrido / patricio_ngg#3563</h4>
+		<hr>
+		<div>
+			<b>Presentación online</b>: <a href="https://bit.ly/39FrK6S">https://bit.ly/39FrK6S</a>
+		</div>
+		<div>
+			<b>Prototipo interactivo</b>: <a href="https://bit.ly/2LYITQE">https://bit.ly/2LYITQE</a>
+		</div>
+		<div>
+			<b>Repositorio Github</b>: <a href="https://github.com/patricio-dsgn/yumi">https://github.com/patricio-dsgn/yumi</a>
+		</div>
+		<hr>
+
+			<style>
+			
+			a{
+				position: relative;
+				display: block;
+				margin-bottom: 20px;
+			}
+			
+			.progress{
+				width: 100px;
+				border: 1px solid #ccc;
+			}
+
+			.bar{
+				position: relative;
+				display: inline-block;
+				height: 20px;
+				width: 100px;
+				border-radius: 5px;
+				background-color: #ccc;
+			}
+
+			.bar div{
+				position: relative;
+				display: block;
+				background-color: #203AD1;
+				height: 100%;
+				font-size: 12px;
+				padding-left: 0px;
+				color: #fff;
+				border-radius: 5px;
+			}
+			
+			.bar.ok div{
+				background-color: #32CD29;
+			}
+
+			.page{
+				position: relative;
+				display: inline-block;
+			}
+
+			.isDisabled {
+				color: currentColor;
+				cursor: not-allowed;
+				opacity: 0.5;
+				text-decoration: none;
+			}
+			</style>
+
+
+	<div class="bar"><div style="width:90%;">90%</div></div>
+	<a class="page" href="./index.html">index.html</a>
+	<br>
+
+	<div class="bar ok"><div style="width:100%;">100%</div></div>
+	<a class="page" href="./elements.html">elements.html</a>
+	<br>
+
+	<div class="bar"><div style="width:0%;">0%</div></div>
+	<a class="page isDisabled" href="./login.html">login.html</a>
+	<br>
+
+	<div class="bar"><div style="width:0%;">0%</div></div>
+	<a class="page isDisabled" href="./register.html">register.html</a>
+	<br>
+
+	<div class="bar"><div style="width:0%;">0%</div></div>
+	<a class="page isDisabled" href="./promo.html">promo.html</a>
+	<br>
+
+	<div class="bar"><div style="width:0%;">0%</div></div>
+	<a class="page isDisabled" href="./profile.html">profile.html</a>
+	<br>
+
+	<div class="bar"><div style="width:0%;">0%</div></div>
+	<a class="page isDisabled" href="./place.html">place.html</a>
+	<br>
+
+	<div class="bar"><div style="width:0%;">0%</div></div>
+	<a class="page isDisabled" href="./map.html">map.html</a>
+	<br>
+
+
+
+	</div>
+
+</div>
+
+</body>
+</html>
